58439e10905081a3d605a7195fb4b31559dee575
Niya Ma <niyama12321@gmail.com>
2023-10-01 14:09:49 -0400

    remove niya folders

--- problem_set_1_niya/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Run this in terminal
-#+ Command to compile c++ program. here i used common one
-g++ -o ex.out "$1" -std=c++11
-./ex.out < in.txt > out.txt
-diff_output=$(diff out.txt sol.txt)
-echo "$diff_output"
-exit 0
